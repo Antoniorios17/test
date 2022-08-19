af62ef3d31120ee6d27e236f77fbd4b9dea7c1f5
antonio <antonio@gmail.com>
2022-08-19 01:22:31 -0400

    third update

--- gitupdater.sh
+++ gitupdater.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+echo ""
 echo "=====================";
 echo "Welcome to GitUpdater";
 echo "=====================";
-echo "Please select one of the options";
+echo ""
+echo "Please select one of the following options";
 echo "";
 echo "1) Update all files";
 echo "2) Update a specific file";
@@ -13,7 +14,7 @@ read -p "---> " answer;
 
 case $answer in
 	1) git add --all ;;
-	2) echo "Please enter the file name with the extension"
+	2) echo "Please enter the file name with the extension" && ls
 		read -p "--->  " file
 		git add "$file"
 		;;
@@ -24,9 +25,21 @@ esac
 echo "Please add the your commit message";
 read -p "--->" message;
 git commit -m "$message";
+social='[0-9]\{3\}-\{0,1\}[0-9]\{2\}-\{0,1\}[0-9]\{4\}'
+#directory=$(pwd)
+for file in * ;
+do if [[ $(grep $social $file) != ""  ]]; 
+	then 
+	echo "************************"
+	echo "*                      *"	
+	echo "*        WARNING       *"
+	echo "*                      *"
+	echo "************************"
+	echo ""
+	echo "$file has sensitive data" 
+	echo "Please remove all sensitive data from $file"
+	exit 1
+	fi;
+done
 
-
-
-
-
-
+git push origin main;
