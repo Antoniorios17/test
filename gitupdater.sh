c628884dfd9324a44f5be16306c878f9618a5fe0
antonio <antonio@gmail.com>
2022-08-19 00:05:13 -0400

    sd

--- gitupdater.sh
+++ gitupdater.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-
-
-echo "====================="
-echo "Welcome to GitUpdater"
-echo "====================="
-echo "Please select one of the options"
-echo ""
-echo "1) Update all files"
-echo "2) Update a specific file"
-echo ""
-read -p "--->";
+echo "=====================";
+echo "Welcome to GitUpdater";
+echo "=====================";
+echo "Please select one of the options";
+echo "";
+echo "1) Update all files";
+echo "2) Update a specific file";
+echo "";
+read -p "--->" answer;
 
 case $answer in
-	1) git add -all;;
-	2)  ;;
-	*) "Invalid answer" ;; 
-
+	1) git add --all;;
+	2) echo "Please enter the file name with the extension"
+		read -p ">>>  " file
+		git add "$file"
+		;;
+	*) echo "Invalid answer" 
+		;; 
 esac
-echo "Please select the file you would like to update"
+
+echo "Please add the your commit message"
+read -p ">>>" message;
 
 
+git commit -m "$message";
